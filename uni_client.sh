40004be22cfdcec0c5a12148d4d6710136a883fc
Caleb <cshorttrun@gmail.com>
2014-06-13 23:33:56 -0700

    realized that the test and thread count was for crawler4j and not emma

--- uni_client.sh
+++ uni_client.sh
@@ -3,5 +3,5 @@ curr=${PWD}
 target_path=$curr/Target_Projects
 emmapath=$curr/Coverage/EMMA/lib
 
-java -XX:-UseSplitVerifier -cp $emmapath/emma.jar emmarun -verbose -r xml -jar $target_path/BasicCrawler.jar
+java -XX:-UseSplitVerifier -cp $emmapath/emma.jar emmarun -verbose -r xml -jar $target_path/BasicCrawler.jar test 1
 python $curr/Coverage/CoverageWrapper.py -f coverage.xml
