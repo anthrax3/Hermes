86e410109092755fdbc5b4cec76bfda5f4b81186
Ubuntu <ubuntu@ip-172-31-0-199.us-west-2.compute.internal>
2014-06-14 07:09:10 +0000

    converted script to unix returns

--- cleaner.sh
+++ cleaner.sh
@@ -1,26 +1,15 @@
-
 rm hermes_f_dump.txt
 rm hermes_eval_client_dump.txt
-
 cd Logs/
-
 sudo rm *.log
-
 cd ..
 cd test/
-
 sudo rm -r frontier/
-
 cd ..
 cd GA/Logs/
-
 sudo rm *.txt
-
 cd ..
 cd Reports/
-
 sudo rm *.xml
-
 cd ..
 cd ..
-
